72a2c56299b5cf2784c6675f79d87dc4ab2577c3
ON-Link <onlink4it@gmail.com>
2018-02-13 19:24:00 +0200

    fix wsgi

--- Smart_Clinic/wsgi.py
+++ Smart_Clinic/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
 
+import os, sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Smart_Clinic.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "Smart_Clinic.settings"
 
 application = get_wsgi_application()
+
